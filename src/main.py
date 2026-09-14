81256c7dec21dffef59cabe11c4e356b45c5b1b2
Chamon Roy <chamonroy@gmail.com>
2026-09-14 18:43:12 +0600

    Add product filtering sorting and aggregation

--- src/main.py
+++ src/main.py
@@ -82,8 +82,10 @@ def fetch_all_products():
         while skip < data["total"]:
             page_data = fetch_data_from_api(skip, data["limit"])
             if page_data is not None and "products" in page_data and page_data["products"] and "limit" in page_data and "skip" in page_data and "total" in page_data:
+
                 all_products.extend(page_data["products"])
                 skip = skip + page_data["limit"]
+                
             else:
                 logging.error(f"Failed to fetch page with skip={skip}. Stopping pagination.")
                 break
@@ -92,4 +94,107 @@ def fetch_all_products():
 
 
 products = fetch_all_products()
-print(len(products))
+# print(len(products))
+# print(products[0]["brand"])
+# print(products[0]["dimensions"]["width"])
+# print(products[0]["meta"]["barcode"])
+
+
+# product = products[0]
+# product = products[15]
+
+def transform_product(product):
+    clean_product = {
+        "id": product["id"],
+        "name": product["title"],
+        "brand": product.get("brand", "Unknown"),
+        "price": product["price"],
+        "rating": product["rating"],
+        "category": product["category"],
+        "width": product["dimensions"]["width"],
+        "barcode": product["meta"]["barcode"]
+    }
+
+    return clean_product
+
+clean_products = [transform_product(product) for product in products]
+
+# print(len(clean_products)) 
+# print(clean_products[15])
+
+
+
+# expensive_products = []
+
+# for product in clean_products:
+#     if product["price"] > 100:
+#         expensive_products.append(product)
+
+
+
+expensive_products = [
+    product for product in clean_products
+    if product["price"] > 100
+]
+
+
+# print(len(expensive_products))
+# print(expensive_products[0])
+
+
+
+
+
+high_value_products = []
+
+for product in clean_products:
+    if product["price"] > 100 and product["rating"] > 4.5:
+        high_value_products.append(product)
+
+
+# print(len(high_value_products))
+# print(high_value_products[0])
+
+
+sorted_products = sorted(
+    clean_products,
+    key=lambda product: product["rating"], 
+    reverse=True
+)
+
+# print(len(sorted_products))
+# print(sorted_products[:5])
+
+
+min_price = min(
+    clean_products,
+    key=lambda product: product["price"]
+)
+
+# print(f"{min_price['name']} is minimum priced product with ${min_price['price']}")
+
+
+max_price = max(
+    clean_products,
+    key=lambda product: product["price"]
+)
+
+# print(f"{max_price['name']} is maximum priced product with ${max_price['price']}")
+
+prices = [product["price"] for product in clean_products]
+total_price = sum(prices)
+avarage_price = total_price / len(clean_products)
+# print(f"Average price: ${avarage_price:.2f}")
+
+
+
+category_counts = {}
+
+for product in clean_products:
+    category = product["category"]
+    if category in category_counts:
+        category_counts[category] += 1
+    else:
+        category_counts[category] = 1
+
+print(category_counts)
